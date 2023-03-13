28545e0b919ee7440919d4bb1738d471243a1caa
ast28 <ariel.sanchez.t28@gmail.com>
2023-03-13 12:15:20 -0600

    login page

--- app.js
+++ app.js
@@ -24,3 +24,44 @@ app.post('/mensaje', (req, res) => {
 app.listen(3000, () => {
   console.log('Servidor iniciado en el puerto 3000');
 });
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+// Configuramos la estrategia de autenticación local
+passport.use(new LocalStrategy(
+  (username, password, done) => {
+    // Aquí debes verificar si el usuario y contraseña son válidos
+    if (username === 'usuario' && password === 'contraseña') {
+      return done(null, { username });
+    } else {
+      return done(null, false);
+    }
+  }
+));
+
+// Configuramos la serialización y deserialización de usuarios
+passport.serializeUser((user, done) => {
+  done(null, user.username);
+});
+
+passport.deserializeUser((username, done) => {
+  done(null, { username });
+});
+
+// Configuramos Passport.js para usar las funciones de serialización y deserialización
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Configuramos una ruta para mostrar la vista de inicio de sesión
+app.get('/login', (req, res) => {
+    res.render('login');
+  });
+  
+  // Configuramos una ruta para manejar las solicitudes de autenticación
+  app.post('/login',
+    passport.authenticate('local', { failureRedirect: '/login' }),
+    (req, res) => {
+      res.redirect('/');
+    }
+  );
